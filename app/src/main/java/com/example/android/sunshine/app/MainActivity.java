4471767ee7a33c5be106ffa7caac0884db505d71
KarthicK Mariappan <karthick1.mariappan@gmail.com>
2016-03-27 09:30:36 -0400

    Feature: Add list view with forecast data using adapter

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,6 +8,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -21,6 +27,7 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
     }
 
     @Override
@@ -48,6 +55,7 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
+
     public static class PlaceholderFragment extends Fragment {
 
         public PlaceholderFragment() {
@@ -57,6 +65,25 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            /*Create Dummy data to populate the list view */
+            String[] forecastArray = {"Today - Sunny - 63/43", "Tomorrow - Rainy - 60/45", "Monday - Breezy - 49/60",
+                                        "Tuesday - Warmer - 65/47", "Wednesday - Pleasant - 69/45", "Thursday - Mild - 64/45",
+                                        "Friday - Partly Sunny - 59/37", "Saturday - Sunny - 65/50",
+                    "Monday - Breezy - 49/60",
+                    "Tuesday - Warmer - 65/47", "Wednesday - Pleasant - 69/45", "Thursday - Mild - 64/45",
+                    "Friday - Partly Sunny - 59/37", "Saturday - Sunny - 65/50"};
+            List<String> weekForecast = new ArrayList<String> (Arrays.asList(forecastArray));
+
+            /*Initiate ArrayAdapter */
+            ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(
+                    getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+
+            /* Get a reference to the list view and attach it to the adapter */
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(forecastAdapter);
+
+
             return rootView;
         }
     }
