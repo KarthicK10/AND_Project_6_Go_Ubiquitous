a025853ee4bb87117eeaed41ea4d007137a55dae
KarthicK Mariappan <karthick1.mariappan@gmail.com>
2016-11-05 07:38:05 -0400

    feat: 5/11 L3: Change icon pack based on user preference.

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -30,8 +30,7 @@ public class SettingsActivity extends PreferenceActivity
          * so the UI summary can update when the preference changes */
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_unit_key)));
-
-
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_art_pack_key)));
     }
 
     //Registers a shared preference change listener that gets notified when preferences change
@@ -121,6 +120,10 @@ public class SettingsActivity extends PreferenceActivity
             //our location status has changed. Update the summary accordingly.
             Preference locationPreference = findPreference(getString(R.string.pref_location_key));
             bindPreferenceSummaryToValue(locationPreference);
+        }
+        else if( key.equals(getString(R.string.pref_art_pack_key))){
+            //art pack changed, update images accordingly
+            getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
         }
     }
 
